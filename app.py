5ac320a1030c8c56feb36927315f1d1e61e9680c
igniteeng000 <igniteeng000@gmail.com>
2018-01-06 19:28:56 +0530

    get and post

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 
 from database import Database
 from menu import Menu
 
 #python flask mongo jinja
+from models.user import User
+
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('login.html')
+
+@app.before_first_request
+def initialize_database():
+    Database.initialize()
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    if User.login.valid(email, password):
+        User.login(email)
+
+    return render_template("profile.html", email = session['email'])
 
 if __name__ == '__main__':
     app.run()
--- models/user.py
+++ models/user.py
@@ -1,4 +1,4 @@
-import datetime from datetime
+from datetime import datetime
 from flask import session
 
 from database import Database
